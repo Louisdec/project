bcdc18ac91234eae9ee6b2fbe5e555c821119d84
Louisdec <35365555+Louisdec@users.noreply.github.com>
2018-01-15 23:46:23 +0100

    Add files via upload

---
+++ Coffre.java
@@ -0,0 +1,19 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Coffre {
+
+	public int PosX, PosY;
+	/** La position du plongeur **/
+	Color couleur;
+
+	public Coffre(int x, int y, Color couleur) {
+
+
+		this.PosX = x;
+		this.PosY = y;
+		this.couleur = couleur;
+
+	}
+
+}
